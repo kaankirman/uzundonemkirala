8c8364e66402ebcadd4661e396817f54c3442da4
Kaan Kırman <kaankirman@outlook.com>
2024-07-01 11:53:09 +0300

    getting lowest segment price data

--- src/components/userClient/Segments.tsx
+++ src/components/userClient/Segments.tsx
@@ -1,19 +1,78 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import luxuryBg from "@public/assets/images/luxBg.jpg";
 import ecoBg from "@public/assets/images/ecoBg.jpg";
 import highBg from "@public/assets/images/highBg.jpg";
 import midBg from "@public/assets/images/midBg.jpg";
 import Link from "next/link";
 import "@styles/segments.css";
+import { DateObject } from "react-multi-date-picker";
+import { CarModel } from "@utils/types";
+import { Loading } from "notiflix";
 
 const Segments = () => {
+  const minDate = new DateObject().add(1, "day");
+  const [carModels, setCarModels] = useState<CarModel[]>([]);
+  const [lowestRentalPerGroup, setLowestRentalPerGroup] = useState<CarModel[]>(
+    []
+  );
+
+  useEffect(() => {
+    Loading.standard();
+    const fetchCarModels = async () => {
+      const response = await fetch(`/api/turev/carPrices`, {
+        method: "POST",
+        body: JSON.stringify({
+          pickupDate: minDate.format("DD-MM-YYYY"),
+          pickupTime: "18:00",
+          rentalDuration: "1",
+          currency: "TL",
+        }),
+      });
+
+      const data = await response.json();
+      setCarModels(data);
+    };
+    fetchCarModels();
+  }, []);
+
+  useEffect(() => {
+    const desiredGroups = ["Lüks", "Orta", "Ekonomik", "Üst"];
+
+    const filteredCarModels = carModels.filter((car) =>
+      desiredGroups.includes(car.group_str)
+    );
+
+    const lowestRentalPerGroup = desiredGroups.map((group) => {
+      const carsInGroup = filteredCarModels.filter(
+        (car) => car.group_str === group
+      );
+      if (carsInGroup.length === 0) return null;
+
+      return carsInGroup.reduce((lowest, car) => {
+        const carTotalRental = parseFloat(car.total_rental || "Infinity");
+        const lowestTotalRental = parseFloat(lowest.total_rental || "Infinity");
+        return carTotalRental < lowestTotalRental ? car : lowest;
+      });
+    });
+
+    const result = lowestRentalPerGroup.filter((car) => car !== null);
+    setLowestRentalPerGroup(result);
+    console.log(result);
+    Loading.remove();
+  }, [carModels]);
+
   return (
     <div className="segment-container">
       <div className="segment-row">
         <div className="segment-card">
           <img src={luxuryBg.src} className="segment-image" />
           <div className="segment-price-container">
-            <h1 className="segment-price">32.250,00 TL</h1>
+            {lowestRentalPerGroup[0] ? (
+              <h1 className="segment-price">
+                {lowestRentalPerGroup[0].total_rental} TL + KDV
+              </h1>
+            ) : null}
             <h1 className="segment-price-desc">başlayan fiyatlar</h1>
           </div>
           <div className="segment-gradient-overlay">
@@ -37,7 +96,11 @@ const Segments = () => {
         <div className="segment-card">
           <img src={highBg.src} className="segment-image" />
           <div className="segment-price-container">
-            <h1 className="segment-price">26.300,00 TL</h1>
+            {lowestRentalPerGroup[3] ? (
+              <h1 className="segment-price">
+                {lowestRentalPerGroup[3].total_rental} TL + KDV
+              </h1>
+            ) : null}
             <h1 className="segment-price-desc">başlayan fiyatlar</h1>
           </div>
           <div className="segment-gradient-overlay">
@@ -65,7 +128,11 @@ const Segments = () => {
         <div className="segment-card">
           <img src={midBg.src} className="segment-image" />
           <div className="segment-price-container">
-            <h1 className="segment-price">21.500,00 TL</h1>
+            {lowestRentalPerGroup[1] ? (
+              <h1 className="segment-price">
+                {lowestRentalPerGroup[1].total_rental} TL + KDV
+              </h1>
+            ) : null}
             <h1 className="segment-price-desc">başlayan fiyatlar</h1>
           </div>
           <div className="segment-gradient-overlay">
@@ -91,7 +158,11 @@ const Segments = () => {
         <div className="segment-card">
           <img src={ecoBg.src} className="segment-image" />
           <div className="segment-price-container">
-            <h1 className="segment-price">20.250,00 TL</h1>
+            {lowestRentalPerGroup[2] ? (
+              <h1 className="segment-price">
+                {lowestRentalPerGroup[2].total_rental} TL + KDV
+              </h1>
+            ) : null}
             <h1 className="segment-price-desc">başlayan fiyatlar</h1>
           </div>
           <div className="segment-gradient-overlay">
